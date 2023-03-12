de8819d58b4d9253ff936660765bd9a566760d33
claudia ciontu <ciontu.claudia.a6e@student.ucv.ro>
2023-03-12 14:57:01 +0200

    chore: updated model post

--- app/src/main/java/com/example/socialmediaapplication/model/ModelPostOnBoard.java
+++ app/src/main/java/com/example/socialmediaapplication/model/ModelPostOnBoard.java
@@ -4,26 +4,115 @@ public class ModelPostOnBoard {
 
     String postTime, postComments;
     String title;
-    String udp;
+    String userDashBoardProfile;
     String userEmail;
     String userId;
     String userImage;
     String description;
     String userName, postLike;
 
-    public ModelPostOnBoard(String postTime, String postComments, String title, String udp, String userEmail, String userId, String userImage, String description, String userName, String postLike) {
+    String pid;
+
+    public ModelPostOnBoard(String postTime, String postComments, String title, String userDashBoardProfile, String userEmail, String userId, String userImage, String description, String userName, String postLike, String profileId) {
         this.postTime = postTime;
         this.postComments = postComments;
         this.title = title;
-        this.udp = udp;
+        this.userDashBoardProfile = userDashBoardProfile;
         this.userEmail = userEmail;
         this.userId = userId;
         this.userImage = userImage;
         this.description = description;
         this.userName = userName;
         this.postLike = postLike;
+        this.pid = profileId;
     }
 
+    public String getPostTime() {
+        return postTime;
+    }
 
+    public void setPostTime(String postTime) {
+        this.postTime = postTime;
+    }
+
+    public String getPostComments() {
+        return postComments;
+    }
+
+    public void setPostComments(String postComments) {
+        this.postComments = postComments;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUserDashBoardProfile() {
+        return userDashBoardProfile;
+    }
+
+    public void setUserDashBoardProfile(String userDashBoardProfile) {
+        this.userDashBoardProfile = userDashBoardProfile;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(String userImage) {
+        this.userImage = userImage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPostLike() {
+        return postLike;
+    }
+
+    public void setPostLike(String postLike) {
+        this.postLike = postLike;
+    }
+
+    public String getPid() {
+        return pid;
+    }
+
+    public void setPid(String pid) {
+        this.pid = pid;
+    }
 }
 
