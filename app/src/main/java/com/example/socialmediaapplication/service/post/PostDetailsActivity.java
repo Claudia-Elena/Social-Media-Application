8073a972cc1e9db72801acf651e01213e479ade9
claudia ciontu <ciontu.claudia.a6e@student.ucv.ro>
2023-03-12 18:45:53 +0200

    frat: add the activity post details

---
+++ app/src/main/java/com/example/socialmediaapplication/service/post/PostDetailsActivity.java
@@ -0,0 +1,14 @@
+package com.example.socialmediaapplication.service.post;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.socialmediaapplication.R;
+
+public class PostDetailsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_details);
+    }
+}
